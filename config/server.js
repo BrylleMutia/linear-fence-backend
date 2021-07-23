f1e800ee2ee72d809f0decf75d9c9222ba7770d3
Brylle Mutia <bryllemutia3@gmail.com>
2021-07-23 15:15:58 +0800

    added route endpoints for api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8bb9179ce344b82f85593439c4545d27'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9483bbf9-8591-4eb9-89ab-7d67b97cdc9a'
+};
